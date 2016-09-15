4c229d3ed1c22f3a1bdaaf2f5493eea1b03e4a7f
cleung494 <christopher.leung@sjsu.edu>
2016-09-15 01:19:02 -0700

    Add files via upload

---
+++ hw2/src/hw2/MailSystemTester.java
@@ -0,0 +1,17 @@
+/*
+ * Christopher Leung
+ * SJSU ID: 008405033
+ */
+package hw2;
+
+/**
+ *
+ * @author chris
+ */
+public class MailSystemTester {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
