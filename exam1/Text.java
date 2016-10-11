f76048f83c2627d88f5b16ffd720d35d55f75026
cleung494 <christopher.leung@sjsu.edu>
2016-10-11 14:36:03 -0700

    Create Text.java

---
+++ exam1/Text.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chris
+ */
+public interface Text {
+    String message();
+    
+    class PlainText implements Text
+    {
+        private String send, receive;
+        int num_times;
+        public PlainText(String sender, String receiver, int num_read)
+        {
+            send = sender;
+            receive = receiver;
+            num_times = num_read;
+        }
+        public String message() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+}
