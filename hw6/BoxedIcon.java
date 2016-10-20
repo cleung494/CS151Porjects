8fdc7b4ddde6d47f95c59849e886f1cc66758b46
cleung494 <christopher.leung@sjsu.edu>
2016-10-19 20:34:57 -0700

    Create BoxedIcon.java

---
+++ hw6/BoxedIcon.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author chris
+ * 
+ * puts a box around an icon
+ */
+public class BoxedIcon implements Icon{
+
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getIconWidth() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getIconHeight() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
