f7d61a98548bc52f7be65268014771431ba9981d
cleung494 <christopher.leung@sjsu.edu>
2016-10-19 22:19:09 -0700

    Update BoxedIcon.java

--- hw6/BoxedIcon.java
+++ hw6/BoxedIcon.java
@@ -6,6 +6,7 @@
 import java.util.*;
 import java.io.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import javax.swing.*;
 
 /**
@@ -18,23 +19,33 @@ public class BoxedIcon implements Icon{
 
     public BoxedIcon(Icon icon, int aSize)
     {
-        size = aSize;
+        pixel_size = aSize;
         image = icon;
     }
     
     @Override
     public void paintIcon(Component c, Graphics g, int x, int y) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (pixel_size == 0)
+        {
+            image.paintIcon(c, g, this.image.getIconWidth(), this.image.getIconHeight());
+        }
+        else{
+            
+        Graphics2D g2 = (Graphics2D) g;
+        Rectangle2D.Double box = new Rectangle.Double(x, y, image.getIconWidth() + pixel_size, + image.getIconHeight() + pixel_size);
+        g2.draw(box);
+        image.paintIcon(c, g, this.image.getIconWidth(), this.image.getIconHeight());
+        }
     }
 
     @Override
     public int getIconWidth() {
-        return (image.getIconHeight() + 2); //because there will be a 1 pixel space and 1 pixel thick line
+        return (image.getIconHeight() + pixel_size); //because there will be a 1 pixel space and 1 pixel thick line
     }
 
     @Override
     public int getIconHeight() {
-        return (image.getIconHeight() + 2);
+        return (image.getIconHeight() + pixel_size);
     }
     private int pixel_size;
     private Icon image;
